76cc72e8cae8103291f89e9f6b9287be3c61d3de
Olufemi Daniel <oladotun.olufemi@yahoo.com>
2020-02-14 18:49:54 +0100

    landing page update

--- js/utility.js
+++ js/utility.js
@@ -27,12 +27,12 @@
     /*-----------------
     scroll-up
     -----------------*/
-    $.scrollUp({
-        scrollText: '<i class="fa fa-arrow-up" aria-hidden="true"></i>',
-        easingType: 'linear',
-        scrollSpeed: 500,
-        animation: 'fade'
-    });
+//    $.scrollUp({
+//        scrollText: '<i class="fa fa-arrow-up" aria-hidden="true"></i>',
+//        easingType: 'linear',
+//        scrollSpeed: 500,
+//        animation: 'fade'
+//    });
 
     /*---------------------
     smooth scroll
@@ -46,40 +46,44 @@
     //        }, 1200);
     //    });
 
+    $('#reveal_subscription').on('click', function() {
+        $('.subscription_reason').toggle('slow')
+    });
+
 
 
     /*---------------------
     testimonial-carousel
     --------------------- */
-    function testimonial_carousel() {
-        var owl = $(".testimonial-carousel");
-        owl.owlCarousel({
-            loop: true,
-            margin: 30,
-            responsiveClass: true,
-            navigation: true,
-            navText: ["<i class='fal fa-long-arrow-left'></i>", "<i class='fal fa-long-arrow-right'></i>"],
-            nav: true,
-            items: 2,
-            smartSpeed: 2000,
-            dots: false,
-            autoplay: false,
-            autoplayTimeout: 4000,
-            center: false,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                480: {
-                    items: 1
-                },
-                760: {
-                    items: 2
-                }
-            }
-        });
-    }
-    testimonial_carousel();
+//    function testimonial_carousel() {
+//        var owl = $(".testimonial-carousel");
+//        owl.owlCarousel({
+//            loop: true,
+//            margin: 30,
+//            responsiveClass: true,
+//            navigation: true,
+//            navText: ["<i class='fal fa-long-arrow-left'></i>", "<i class='fal fa-long-arrow-right'></i>"],
+//            nav: true,
+//            items: 2,
+//            smartSpeed: 2000,
+//            dots: false,
+//            autoplay: false,
+//            autoplayTimeout: 4000,
+//            center: false,
+//            responsive: {
+//                0: {
+//                    items: 1
+//                },
+//                480: {
+//                    items: 1
+//                },
+//                760: {
+//                    items: 2
+//                }
+//            }
+//        });
+//    }
+//    testimonial_carousel();
 
 
 }(jQuery));
